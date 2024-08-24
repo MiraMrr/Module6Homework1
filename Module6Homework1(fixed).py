40eb19dfc4518caca88fdcc50e8d5d532cd3cc37
Karina <psinka.94@gmail.com>
2024-08-25 02:49:31 +0300

    Add files via upload
    Исправлено - метод eat перенесен в родительский класс

---
+++ Module6Homework1(fixed).py
@@ -0,0 +1,53 @@
+class Animal:
+    def __init__(self, name):
+        self.alive = True
+        self.fed = False
+        self.name = name
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+class Plant:
+    def __init__(self, name):
+        self.edible = False
+        self.name = name
+
+
+class Mammal(Animal):
+    pass
+
+
+class Predator(Animal):
+    pass
+
+class Flower(Plant):
+    pass
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+
+
+
+a1 = Predator('Белый медведь')
+a2 = Mammal('Слон')
+p1 = Flower('Роза')
+p2 = Fruit('Арбуз')
+
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+
+a1.eat(p1)
+a2.eat(p2)
+
+print(a1.alive)
+print(a2.fed)
